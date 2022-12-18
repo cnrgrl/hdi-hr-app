c42ddd485674fcd3c480f4c8153f33d09bfc5930
caner <caner@quizco.de>
2022-12-18 22:11:33 +0100

    fix: app and authlayout tidied up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import {Provider} from "react-redux";
 import Router from "./config/router";
 import {store} from "./redux";
+import {CssBaseline} from "@mui/material";
 
-function App() {
+export default function App() {
   return (
     <Provider store={store}>
+      <CssBaseline />
       <Router />
     </Provider>
   );
 }
-
-export default App;
--- src/layout/AuthLayout.js
+++ src/layout/AuthLayout.js
@@ -1,16 +1,27 @@
 import {Outlet, Navigate} from "react-router-dom";
-import {Container} from "@mui/material";
+import {Container, Avatar, Typography} from "@mui/material";
+import {Lock as LockIcon} from "feather-icons-react";
+import {useSelector} from "react-redux";
 import {useIsLoggedIn} from "../config/hooks";
 
 export default function AuthLayout() {
   const isLoggedIn = useIsLoggedIn();
+
+  const error = useSelector((state) => state.auth.error);
 
   if (isLoggedIn === null) return <h1>Loading...</h1>;
   else if (isLoggedIn === true) return <Navigate replace to="/" />;
 
   return (
     <Container maxWidth="xs" sx={{pt: 4}}>
-      Hi Caner, you logged in!
+      <Avatar sx={{mx: "auto", bgcolor: "secondary.main"}}>
+        <LockIcon style={{color: "white"}} />
+      </Avatar>
+      {error && (
+        <Typography sx={{textAlign: "center", color: "error.main"}}>
+          {error}
+        </Typography>
+      )}
       <Outlet />
     </Container>
   );
