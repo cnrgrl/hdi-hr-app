c6b070090dfd1db25baaf16f622ac3d188c27b8f
caner <caner@quizco.de>
2022-12-18 17:38:06 +0100

    main layout added and  tested

--- src/config/router.js
+++ src/config/router.js
@@ -3,12 +3,15 @@ import Home from "../pages/Home";
 import SignUp from "../pages/SignUp";
 import SignIn from "../pages/SignIn";
 import AuthLayout from "../layout/AuthLayout";
+import Layout from "../layout/Layout";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route element={<Layout />}>
+          <Route path="/" element={<Home />} />
+        </Route>
         <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
---
+++ src/layout/Layout.js
@@ -0,0 +1,17 @@
+import {Outlet, Navigate} from "react-router-dom";
+import {Container} from "@mui/material";
+import {useIsLoggedIn} from "../config/hooks";
+
+export default function Layout() {
+  const isLoggedIn = useIsLoggedIn();
+
+  if (isLoggedIn === null) return <h1>Loading...</h1>;
+  else if (isLoggedIn === false) return <Navigate replace to="/sign-in" />;
+
+  return (
+    <Container maxWidth="xs" sx={{pt: 4}}>
+      Hi Caner, you logged in!
+      <Outlet />
+    </Container>
+  );
+}
