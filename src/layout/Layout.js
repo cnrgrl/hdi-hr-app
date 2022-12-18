cf257c013fb5ba7e2f859bf209b3ee2b9cacd155
caner <caner@quizco.de>
2022-12-18 22:37:50 +0100

    feat: app bar added

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,17 +1,66 @@
 import {Outlet, Navigate} from "react-router-dom";
-import {Container} from "@mui/material";
+import {
+  AppBar,
+  Toolbar,
+  Typography,
+  IconButton,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import {User as UserIcon} from "feather-icons-react";
 import {useIsLoggedIn} from "../config/hooks";
+import {useState} from "react";
 
 export default function Layout() {
   const isLoggedIn = useIsLoggedIn();
+  const [anchorEl, setAnchorEl] = useState(null);
 
   if (isLoggedIn === null) return <h1>Loading...</h1>;
   else if (isLoggedIn === false) return <Navigate replace to="/sign-in" />;
 
   return (
-    <Container maxWidth="xs" sx={{pt: 4}}>
-      Hi Caner, you logged in!
+    <>
+      {" "}
+      <AppBar sx={{position: "static", marginBottom: 6}}>
+        <Toolbar>
+          <Typography variant="h6" sx={{flexGrow: 1}}>
+            Home
+          </Typography>
+          <IconButton
+            color="inherit"
+            size="large"
+            onClick={(e) => {
+              setAnchorEl(e.currentTarget);
+            }}
+          >
+            <UserIcon />
+          </IconButton>
+
+          <Menu
+            open={Boolean(anchorEl)}
+            anchorEl={anchorEl}
+            onClose={() => {
+              setAnchorEl(null);
+            }}
+          >
+            <MenuItem
+              onClick={() => {
+                setAnchorEl(null);
+              }}
+            >
+              Profile
+            </MenuItem>
+            <MenuItem
+              onClick={() => {
+                setAnchorEl(null);
+              }}
+            >
+              New Record
+            </MenuItem>
+          </Menu>
+        </Toolbar>
+      </AppBar>
       <Outlet />
-    </Container>
+    </>
   );
 }
